7f04ecc6b31024166bd147eea57a2eadc7c378b4
Gorokhov Denis <gorokhov1love@gmail.com>
2022-12-04 12:30:37 +0300

    Optimized method click

--- addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook_web_tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -16,11 +16,13 @@ public class HelperBase {
 
     protected void type(By locator, String text) {
         click(locator);
-        if (text != null){
-            driver.findElement(locator).clear();
-            driver.findElement(locator).sendKeys(text);
+        if (text != null) {
+            String existingText = driver.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                driver.findElement(locator).clear();
+                driver.findElement(locator).sendKeys(text);
+            }
         }
-
     }
 
     private String closeAlertAndGetItsText() {
